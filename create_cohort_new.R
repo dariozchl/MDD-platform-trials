9e7c58de2461f2fcd873f36503523f1af5d7ed3a
dariozchl <dario.zocholl@charite.de>
2021-05-18 12:41:51 +0200

    dopped unneccessary comment

---
+++ create_cohort_new.R
@@ -0,0 +1,50 @@
+
+#' Helper Function: Create new cohort and update allocation ratio
+#' @export
+create_cohort_new <- function(res_list, n_int, n_fin, sharing_type, treatment_effects, way_of_administration, applicable_to_TRD=TRUE, applicable_to_PRD=TRUE) {
+
+
+  name_placeholder <- paste0(way_of_administration,"_Treatment") # required for randomly drawing response
+
+  if(applicable_to_TRD){
+    new_list <- list(list(decision = rep("none", 2), alloc_ratio = NULL, n_thresh = NULL, start_n =  total_n(res_list$TRD),
+                          response = treatment_effects$TRD[[paste(name_placeholder)]][[sample(x=1:(length(treatment_effects$TRD[[paste(name_placeholder)]])-1), size=1)]],
+                          endpoint = NULL,
+                          n = rep(NA, length(res_list$TRD[[paste0(way_of_administration, "_Control")]]$n))))
+    # assign name to the new treatment: find highest already existing treatment index among this way of administration and add +1
+    names_of_treatments <- c(names(res_list$TRD), names(res_list$PRD))[grep(paste0(way_of_administration,"_Treatment"),c(names(res_list$TRD), names(res_list$PRD)))]
+    number_treatments_max <- max(as.numeric(gsub(pattern=paste0(way_of_administration,"_Treatment"), replacement="", x=names_of_treatments[grep(way_of_administration, names_of_treatments)])))
+    new_name <- paste0(way_of_administration,"_Treatment",number_treatments_max+1)
+
+    res_list$TRD <- c(res_list$TRD, new_list)
+    names(res_list$TRD)[length(res_list$TRD)] <- new_name
+  }
+
+  if(applicable_to_PRD){
+    new_list <- list(list(decision = rep("none", 2), alloc_ratio = NULL, n_thresh = NULL, start_n =  total_n(res_list$PRD),
+                          response = treatment_effects$PRD[[paste(name_placeholder)]][[sample(x=1:(length(treatment_effects$PRD[[paste(name_placeholder)]])-1), size=1)]],
+                          endpoint = NULL,
+                          n = rep(NA, length(res_list$PRD[[paste0(way_of_administration, "_Control")]]$n))))
+
+    # assign name to the new treatment: find highest already existing treatment index among this way of administration and add +1
+    names_of_treatments <- c(names(res_list$TRD), names(res_list$PRD))[grep(paste0(way_of_administration,"_Treatment"),c(names(res_list$TRD), names(res_list$PRD)))]
+    number_treatments_max <- max(as.numeric(gsub(pattern=paste0(way_of_administration,"_Treatment"), replacement="", x=names_of_treatments[grep(way_of_administration, names_of_treatments)])))
+    new_name <- paste0(way_of_administration,"_Treatment",number_treatments_max+1)
+
+    res_list$PRD <- c(res_list$PRD, new_list)
+    names(res_list$PRD)[length(res_list$PRD)] <- new_name
+
+  }
+
+    # Update allocation ratio
+  res_list <- update_alloc_ratio(res_list)
+
+  return(res_list)
+}
+
+
+
+res_list <- create_cohort_new(res_list, n_int=50, n_fin=100, sharing_type=sharing_type, treatment_effects=treatment_effects,
+                              way_of_administration="IV", applicable_to_TRD=TRUE, applicable_to_PRD=TRUE)
+View(res_list)
+
