bf84468e087509dc35566791993ab6722d6a8848
dariozchl <dario.zocholl@charite.de>
2021-05-18 14:01:15 +0200

    removed helper functions from the script

---
+++ coh_left_check.R
@@ -0,0 +1,13 @@
+
+#' Helper Function: Check which cohorts are left
+#' @export
+coh_left_check <- function(x) {
+  cohorts_left_TRD <- sapply(x$TRD, function(y) {y$decision[1] %in% c("none", "PROMISING", "CONTINUE") & y$decision[2] == "none"})
+  cohorts_left_PRD <- sapply(x$PRD, function(y) {y$decision[1] %in% c("none", "PROMISING", "CONTINUE") & y$decision[2] == "none"})
+  rbind(t(as.data.frame(cohorts_left_TRD)), t(as.data.frame(cohorts_left_PRD)))
+
+  return(cbind(as.data.frame(cohorts_left_TRD), as.data.frame(cohorts_left_PRD)))
+}
+
+coh_left_check(x=res_list)
+
