10c6defd8d5eda46b48b40e2dd766f2fd427ee37
Dario <dario.zocholl@charite.de>
2021-06-17 11:15:30 +0200

    deleted unneccessary line

--- coh_left_check.R
+++ coh_left_check.R
@@ -4,7 +4,6 @@
 coh_left_check <- function(x) {
   cohorts_left_TRD <- sapply(x$TRD, function(y) {y$decision[1] %in% c("none", "PROMISING", "CONTINUE") & y$decision[2] == "none"})
   cohorts_left_PRD <- sapply(x$PRD, function(y) {y$decision[1] %in% c("none", "PROMISING", "CONTINUE") & y$decision[2] == "none"})
-  rbind(t(as.data.frame(cohorts_left_TRD)), t(as.data.frame(cohorts_left_PRD)))
 
   return(cbind(as.data.frame(cohorts_left_TRD), as.data.frame(cohorts_left_PRD)))
 }
